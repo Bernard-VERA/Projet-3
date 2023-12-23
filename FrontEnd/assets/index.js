70853f520c75c42c8da9fb4ba48366a1e152b8b5
Bernard-VERA <bvmars@hotmail.com>
2023-12-23 16:22:20 +0100

    Filtres des projets finis

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -2,27 +2,17 @@
 const container = document.querySelector(".gallery");
 console.log(container);
 
+let allWorks = [];
 const getWorks = () => {
     fetch("http://localhost:5678/api/works")
         .then(function(response) {
             if(response.ok) {
             return response.json();
-         }
+        }
         })
         .then(function(data) {
-            let works = data;
-            console.log(works);
-            works.forEach((work, index) => {
-                let newFigure = document.createElement('figure');
-            let newImage = document.createElement('img');
-                newImage.setAttribute('src', work.imageUrl);
-                newImage.setAttribute('alt', work.title);
-                newFigure.appendChild(newImage);
-                let newFigcaption = document.createElement('figcaption');
-                newFigcaption.innerText = work.title;
-                newFigure.appendChild(newFigcaption);
-            document.querySelector(".gallery").appendChild(newFigure);
-            });
+            allWorks = data
+           showWorks()
         })
         .catch(function(error) {
             console.log(error);
@@ -30,6 +20,34 @@ const getWorks = () => {
     }
 getWorks()
 
+const showWorks = (id = null) => {
+    document.querySelector(".gallery").innerHTML = ""
+    let works = allWorks
+        if (id) {
+            works = allWorks.filter((work) => {
+            return work.categoryId == id
+            }) 
+        }
+   
+    works.forEach((work, index) => {
+        let newFigure = document.createElement('figure');
+        let newImage = document.createElement('img');
+            newImage.setAttribute('src', work.imageUrl);
+            newImage.setAttribute('alt', work.title);
+            newFigure.appendChild(newImage);
+        let newFigcaption = document.createElement('figcaption');
+            newFigcaption.innerText = work.title;
+            newFigure.appendChild(newFigcaption);
+        document.querySelector(".gallery").appendChild(newFigure);
+    });
+}
+
+let newButton = document.createElement('button');
+    newButton.innerText = "Tous";
+    document.querySelector(".category").appendChild(newButton);
+    newButton.addEventListener("click",() => {
+    showWorks()
+    })
 
 const getCategory = () => {
     fetch("http://localhost:5678/api/categories")
@@ -40,11 +58,14 @@ const getCategory = () => {
         })
         .then(function(data) {
             let categories = data;
-            console.log(categories);
-            categories.forEach((category, index) => {
-                let newButton = document.createElement('button');
+                console.log(categories);
+                categories.forEach((category, index) => {
+            let newButton = document.createElement('button');
                 newButton.innerText = category.name;
-            document.querySelector(".category").appendChild(newButton);
+                document.querySelector(".category").appendChild(newButton);
+                newButton.addEventListener("click",() => {
+                    showWorks(category.id)
+                })
             })
         })
     }
